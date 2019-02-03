87b7174ff9a007ce98733fc96882c4590fc11531
Tony Oreglia <tony.oreglia@safetyculture.io>
2019-02-03 20:55:29 +1100

    added starting position. Removed old code

--- chess-board/src/index.js
+++ chess-board/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-  let button = <button className="square" onClick={props.onClick}>
+  let button = <button className="light-square" onClick={props.onClick}>
     {props.value}
   </button>
   let row = ~~(props.index / 8)
   if ((props.index%2 + (row%2))%2 !== 0) {
     button = <button className="dark-square" onClick={props.onClick}>
       {props.value}
+    </button>
   }
   return (button);
 }
@@ -50,77 +51,36 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    const blackStartingPos = [
+      '♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜',
+      '♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟']
+    const whiteStartingPos = [
+      '♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙',
+      '♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖']
     this.state = {
       history: [{
-        squares: Array(9).fill(null),
+        squares: blackStartingPos.concat(Array(32).fill(null), whiteStartingPos)
       }],
-      stepNumber: 0,
-      xIsNext: true
+      stepNumber: 0
     };
   }
 
   jumpTo(step) {
     this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      stepNumber: step
     });
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
+    return;
   }
 
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
 
-    let moves = history.map((step, move) => {
-      let mvIndex = -1;
-      step.squares.find((sq) => {
-        mvIndex++;
-        if (move === 0) { return sq != null; }
-        return sq !== history[move-1].squares[mvIndex];
-      });
-      let desc;
-      if (move === this.state.stepNumber) {
-        desc = move ?
-          <b>{'Go to move #' + move + ' (' + (mvIndex % 3 + 1) + ', ' + (~~(mvIndex / 3) + 1) + ')'}</b>:
-          <b>{'Go to game start'}</b>; 
-      }
-      else {
-        desc = move ?
-          'Go to move #' + move + ' (' + (mvIndex % 3 + 1) + ', ' + (~~(mvIndex / 3) + 1) + ')':
-          'Go to game start';
-      }
-      return (
-        <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      );
-    });
-
-    
     let board;
-    let status;
 
-    if (history.length === 10 && this.state.stepNumber === 9) {
-      status = 'CATS game!';
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
     board = <Board
       squares={current.squares}
       onClick={(i) => this.handleClick(i)}
@@ -130,10 +90,6 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           {board}
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          {moves}
         </div>
       </div>
     );
